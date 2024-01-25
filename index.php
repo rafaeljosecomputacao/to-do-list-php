2f378d0a32a5532c315084c05c0b88a1291821da
rafaeljosecomputacao <132665090+rafaeljosecomputacao@users.noreply.github.com>
2024-01-25 01:53:16 -0300

    create-css

--- index.php
+++ index.php
@@ -3,35 +3,42 @@
     include_once("templates/header.php");
 ?>
     <!-- Main -->
-    <main class="to-do">
+    <main class="to-do-container">
+        <!-- Header -->
         <section class="to-do-header">
             <h1>Things to do</h1>
             <form action="" class="to-do-form">
                 <input type="text" name="description" placeholder="Write your task here" required>
-                <button type="submit" class="form-button">
+                <button type="submit" class="form-button create-button">
                     <i class="bi bi-plus"></i>
                 </button>
             </form>
         </section>
-        <section class="to-do-tasks">
-            <div class="task">
+        <!-- List -->
+        <section class="to-do-list">
+            <div class="to-do-task">
                 <input type="checkbox" name="progress" class="task-progress">
-                <p class="task-description">Task 1</p>
-            </div>
-            <div class="task-actions">
-                <button type="button" class="action-button edit-button">
-                    <i class="bi bi-pen"></i>
-                </button>
-                <button type="button" class="action-button delete-button">
-                    <i class="bi bi-trash"></i>
-                </button>
-            </div>
-            <form action="" class="to-do-form task-edit-form">
-                <input type="text" name="description" placeholder="Edit your task here">
-                <button type="submit" class="form-button confirm-button">
-                    <i class="bi bi-check"></i>
-                </button>
-            </form>
+                <p class="task-description">My task one</p>
+                <div class="task-actions">
+                    <button type="submit" class="action-button update-button">
+                        <i class="bi bi-pen"></i>
+                    </button>
+                    <button type="submit" class="action-button delete-button">
+                        <i class="bi bi-trash"></i>
+                    </button>
+                </div>
+                <form action="" class="to-do-form task-form hidden">
+                    <input type="text" name="description" placeholder="Edit your task here">
+                    <button type="submit" class="form-button update-button">
+                        <i class="bi bi-check"></i>
+                    </button>
+                </form>
+            </div>         
+        </section>
+        <!-- Footer -->
+        <section class="to-do-footer">
+            <img src="img/logo.png" alt="Logo">
+            <p>My tasks</p>
         </section>    
     </main>
 <?php
